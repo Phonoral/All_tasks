98a806f6f27ad9dd2b65274896cf4442d8e1321e
Dmitry <berezovsky.py@gmail.com>
2022-04-15 21:43:28 +0300

    Add two tasks

---
+++ InfiniteSeries/InfiniteSeries.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  setlocale(LC_ALL, "Russian"); /// Русский в консоли
+  double summ = 0.0; /// Сумма ряда
+  double index = 0.0; /// Глубина расчета
+  double accuracy = pow(10.0, -3.0); /// Точность
+
+  double var_x = 0.0; /// Х
+  
+  /// Ввод Х
+  cout << "Введите x: ";
+  cin >> var_x;
+  cout << endl;
+
+  /// Проверка введенных данных
+  if (var_x > 1.0)
+  {
+    cout << "Ошибка: x должен быть < 1" << endl;
+    return -1;
+  }
+
+  /// Вычисляем сумму ряда
+  for (double i = 1; pow(var_x, i) / i > accuracy; i++)
+  {
+    summ += pow(var_x, i) / i;
+    index = i;
+  }
+
+  /// Вывод результата
+  cout << "Значение ряда: " << summ << endl;
+  cout << "Глубина расчетов: " << (int)index << endl;
+
+  return 0;
+}
---
+++ MatrixColumn/MatrixColumn.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  setlocale(LC_ALL, "Russian"); // Русский в консоли
+
+  int ** matrix = nullptr; // Указатель на указатель (для динамического выделения памяти)
+  int * B = nullptr; // Результирующий массив
+  int N = 0; // Строки
+  int M = 0; // Столбцы
+
+  // Ввод параметров
+  cout << "Введите количество строк N: ";
+  cin >> N;
+  cout << endl << "Введите количество столбцов M: ";
+  cin >> M;
+  cout << endl;
+
+  // Выделение памяти
+  matrix = new int * [N]; /// Строки
+  for (unsigned i = 0; i < N; i++) matrix[i] = new int[M]; // Столбцы
+  B = new int[M];
+  // Ввод матрицы (построчно)
+  for (unsigned i = 0; i < N; i++)
+  {
+    for (unsigned j = 0; j < M; j++)
+    {
+      cout << "Введите " << j + 1 << " элемент " << i + 1 << " строки матрицы: ";
+      cin >> matrix[i][j];
+      cout << endl;
+    }
+  }
+  // Вывод матрицы
+  cout << "Матрицы:" << endl;
+  for (unsigned i = 0; i < N; i++)
+  {
+    for (unsigned j = 0; j < M; j++)
+    {
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
+  // Рассчеты
+  for (unsigned j = 0; j < M; j++)
+  {
+    int sum = 0;
+
+    for (unsigned i = 0; i < N; i++)
+    {
+      sum += abs(matrix[i][j]);
+    }
+    
+    if (sum == 0) B[j] = 0;
+    else B[j] = 1;
+  }
+  // Вывод результата
+  cout << "Массив В: ";
+  for (unsigned j = 0; j < M; j++) cout << B[j] << " ";
+
+  // Очистка выделенной памяти
+  for (unsigned i = 0; i < N; i++) delete[] matrix[i];
+  delete[] matrix;
+  delete[] B;
+}
+
